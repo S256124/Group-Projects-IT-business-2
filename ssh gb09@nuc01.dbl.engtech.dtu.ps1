eaf3511f92c095aac5f2ab79139a5c049f7c7728
S256124 <s256124@dtu.dk>
2026-04-16 16:41:13 +0200

    database

---
+++ ssh gb09@nuc01.dbl.engtech.dtu.ps1
@@ -0,0 +1,10 @@
+ssh gb09@nuc01.dbl.engtech.dtu.dk
+gb09@nuc01:~$ cd public
+gb09@nuc01:~/public$ ls
+gb09@nuc01:~/public$ mariadb -u gb09_DB -p gb09_DB_password
+SHOW TABLES;
+DESCRIBE users;
+
+
+
+
