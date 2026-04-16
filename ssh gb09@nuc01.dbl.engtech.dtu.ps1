032a505eac2766b3b7cf2a5077c76ac23e7ac3e3
S256124 <s256124@dtu.dk>
2026-04-16 16:57:06 +0200

    DATABASE FINAL 1.0

--- ssh gb09@nuc01.dbl.engtech.dtu.ps1
+++ ssh gb09@nuc01.dbl.engtech.dtu.ps1
@@ -4,7 +4,25 @@ gb09@nuc01:~/public$ ls
 gb09@nuc01:~/public$ mariadb -u gb09_DB -p gb09_DB_password
 SHOW TABLES;
 DESCRIBE users;
-
-
-
+CREATE TABLE recipes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)  UNIQUE KEY NOT NULL,
+    rating FLOAT NOT NULL,
+    times_clicked INT NOT NULL, 
+    time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+)
+CREATE TABLE ratings (
+    rating_id INT AUTO_INCREMENT PRIMARY KEY,
+    recipe_id INT NOT NULL,
+    user_id INT NOT NULL FOREIGN KEY (user_id) REFERENCES users(id),
+    rating FLOAT NOT NULL,
+    time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+);
+CREATE TABLE your_choice (
+    favorite_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL FOREIGN KEY (user_id) REFERENCES users(id),
+    recipe_id INT NOT NULL FOREIGN KEY (recipe_id) REFERENCES recipes(id),
+    time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
